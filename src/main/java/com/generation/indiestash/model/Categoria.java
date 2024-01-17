f703bef98e68562a740eeb6147cfacc07117ae73
Yann <yannssouza@outlook.com>
2024-01-17 18:19:59 -0300

    Correção

--- src/main/java/com/generation/indiestash/model/Categoria.java
+++ src/main/java/com/generation/indiestash/model/Categoria.java
@@ -19,7 +19,7 @@ public class Categoria {
     private Long id;
 
     @NotBlank(message = "O atributo nome é Obrigatório!")
-    @Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+    @Size(min = 1, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
     private String nome;
 
     @NotBlank(message = "O atributo descrição é Obrigatório!")
